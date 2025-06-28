096d0f2a4e6359d809a3cade814f6155112a630c
AlexiaPT <apalaciost@pucp.edu.pe>
2025-06-27 19:24:18 -0500

    v1

---
+++ L10_20211049/src/main/java/com/example/l10_20211049/L1020211049Application.java
@@ -0,0 +1,13 @@
+package com.example.l10_20211049;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1020211049Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1020211049Application.class, args);
+    }
+
+}
---
+++ L10_20211049/src/test/java/com/example/l10_20211049/L1020211049ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l10_20211049;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1020211049ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
